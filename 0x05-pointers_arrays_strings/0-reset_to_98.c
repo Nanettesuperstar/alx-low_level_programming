d77da2d51ab44163c9af0ac35d3b042b3857ca1c
Nanettesuperstar <sisesta@yahoo.com>
2023-05-03 12:14:40 -0700

    0x05-C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - take a pointer to an int as parameter and updates the value it points to to 98
+ * @n1: the value to be updated to 98
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
